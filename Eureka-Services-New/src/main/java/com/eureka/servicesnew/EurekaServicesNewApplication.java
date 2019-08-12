589d29d7ceb6e0fbc7d639020998a7b5fdbbadb7
sayli <saylikumbhar31@gmail.com>
2019-08-12 13:42:52 +0530

    New Commit of Microservices project

---
+++ Eureka-Services-New/src/main/java/com/eureka/servicesnew/EurekaServicesNewApplication.java
@@ -0,0 +1,15 @@
+package com.eureka.servicesnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class EurekaServicesNewApplication 
+{
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServicesNewApplication.class, args);
+	}
+
+}
